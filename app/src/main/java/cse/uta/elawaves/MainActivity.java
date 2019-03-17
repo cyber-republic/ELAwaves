a24417541f3a096e42bb6f5f95a5d5ef790ae633
Dannelor <masonbeckham@yahoo.com>
2019-03-16 21:21:26 -0500

    Wallet implementation and move Fragments into subfolder

--- app/src/main/java/cse/uta/elawaves/MainActivity.java
+++ app/src/main/java/cse/uta/elawaves/MainActivity.java
@@ -1,14 +1,20 @@
 package cse.uta.elawaves;
 
+import android.content.Context;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.elastos.spvcore.ElastosWalletUtils;
+import com.elastos.spvcore.WalletException;
 
 import org.elastos.carrier.exceptions.CarrierException;
 
 import cse.uta.elawaves.Carrier.CarrierCallback;
 import cse.uta.elawaves.Carrier.CarrierImplementation;
 import cse.uta.elawaves.Carrier.CarrierMessage;
+import cse.uta.elawaves.Fragments.FragmentMain;
+import cse.uta.elawaves.Wallet.WalletImplementation;
 
 public class MainActivity extends AppCompatActivity implements FragmentMain.OnFragmentInteractionListener{
 
@@ -17,8 +23,10 @@ public class MainActivity extends AppCompatActivity implements FragmentMain.OnFr
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Startup Carrier
+        Context applicationContext = getApplicationContext();
         try{
-            CarrierImplementation.getInstance(getApplicationContext());
+            CarrierImplementation.getInstance(applicationContext);
         } catch (CarrierException e) {
             e.printStackTrace();
         }
@@ -31,6 +39,15 @@ public class MainActivity extends AppCompatActivity implements FragmentMain.OnFr
                 }
             }
         });
+
+        // Startup Wallet
+        String rootPath = getApplicationContext().getFilesDir().getParent();
+        try{
+            ElastosWalletUtils.InitConfig(this,rootPath);
+            WalletImplementation.getInstance(rootPath);
+        }catch (WalletException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
