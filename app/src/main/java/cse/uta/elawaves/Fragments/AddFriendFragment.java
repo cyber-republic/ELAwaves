91c99614af819a912839516eeda32d468ee8e0e8
Mason <masonbeckham@yahoo.com>
2019-03-20 11:33:38 -0500

    AddFriendFragment boilerplate and My Info navbar

--- app/src/main/java/cse/uta/elawaves/Fragments/AddFriendFragment.java
+++ app/src/main/java/cse/uta/elawaves/Fragments/AddFriendFragment.java
@@ -1,14 +1,23 @@
 package cse.uta.elawaves.Fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.elastos.carrier.FriendInfo;
+
 import cse.uta.elawaves.R;
 
 public class AddFriendFragment extends Fragment {
+
+    OnAddFriendFragmentListener callback;
+
+    public interface OnAddFriendFragmentListener {
+        void onAddFriend(FriendInfo info);
+    }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState){
@@ -21,5 +30,20 @@ public class AddFriendFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onAttach(Context context){
+        super.onAttach(context);
+
+        if(context instanceof OnAddFriendFragmentListener)
+            callback = (OnAddFriendFragmentListener) context;
+        else
+            throw new ClassCastException(context.toString() + " must implement OnFriendSelectedListener");
+    }
+
+    @Override
+    public void onDetach(){
+        callback = null;
+        super.onDetach();
+    }
 
 }
