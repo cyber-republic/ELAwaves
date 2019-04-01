bead4227858de60e54390aa84681a3b7f61a441e
Mason <masonbeckham@yahoo.com>
2019-04-01 12:00:01 -0500

    Fix bottom bar navigation

--- app/src/main/java/cse/uta/elawaves/HomeActivity.java
+++ app/src/main/java/cse/uta/elawaves/HomeActivity.java
@@ -17,12 +17,13 @@ import android.view.View;
 
 import org.elastos.carrier.FriendInfo;
 
+import androidx.navigation.Navigation;
 import cse.uta.elawaves.Fragments.AccountInfoFragment;
 import cse.uta.elawaves.Fragments.AddFriendFragment;
 import cse.uta.elawaves.Fragments.FriendInfoFragment;
 import cse.uta.elawaves.Fragments.FriendsFragment;
 
-public class HomeActivity extends AppCompatActivity implements FriendsFragment.OnFriendSelectedListener, AddFriendFragment.OnAddFriendFragmentListener {
+public class HomeActivity extends AppCompatActivity implements FriendsFragment.OnFriendSelectedListener, AddFriendFragment.OnAddFriendFragmentListener, BottomNavigationView.OnNavigationItemSelectedListener {
 
     private FriendInfo current_chat_friend;
 
@@ -31,7 +32,7 @@ public class HomeActivity extends AppCompatActivity implements FriendsFragment.O
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         BottomNavigationView menu = findViewById(R.id.home_navigation);
-        menu.setOnNavigationItemSelectedListener(navListener);
+        menu.setOnNavigationItemSelectedListener(this);
 
 
     }
@@ -46,28 +47,19 @@ public class HomeActivity extends AppCompatActivity implements FriendsFragment.O
 
     }
 
-
-
-    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-            Fragment selectedFragment = null;
-            switch(menuItem.getItemId()) {
-                case R.id.friends_item:
-                    selectedFragment = new FriendsFragment();
-                    break;
-                case R.id.myinfo_item:
-                    selectedFragment = new AccountInfoFragment();
-                    break;
-                case R.id.wallet_item:
-                    selectedFragment = new AccountInfoFragment();
-                    break;
-            }
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
-            return true;
-
-            }
-
-    };
-
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch(menuItem.getItemId()) {
+            case R.id.friends_item:
+                Navigation.findNavController(this,R.id.nav_host_fragment_home).navigate(R.id.friendsFragment);
+                break;
+            case R.id.myinfo_item:
+                Navigation.findNavController(this,R.id.nav_host_fragment_home).navigate(R.id.accountInfoFragment);
+                break;
+            case R.id.wallet_item:
+                Navigation.findNavController(this,R.id.nav_host_fragment_home).navigate(R.id.accountInfoFragment);
+                break;
+        }
+        return true;
+    }
 }
