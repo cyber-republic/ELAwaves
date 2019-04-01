700c9db076c57d61bd8d5c3b8089a5b4dbb999b2
Brandon Tran <brandon.tran@mavs.uta.edu>
2019-04-01 06:20:01 -0500

    able to create bitmap for a  string and appear on account info page; need to get userInfo in order to generate correct bitmap

---
+++ app/src/main/java/cse/uta/elawaves/Fragments/AccountInfoFragment.java
@@ -0,0 +1,122 @@
+package cse.uta.elawaves.Fragments;
+import android.content.Context;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+
+import android.os.Bundle;
+
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import org.elastos.carrier.Carrier;
+import org.elastos.carrier.UserInfo;
+
+import cse.uta.elawaves.R;
+public class AccountInfoFragment extends Fragment {
+
+    public final static int WIDTH = 500;
+    public static int white = 0xFFFFFFFF;
+    public static int black = 0xFF000000;
+
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_account_info,container,false);
+        ImageView imageView = view.findViewById(R.id.profilePic);
+        ImageView qrCodePic = view.findViewById(R.id.userQRCode);
+        imageView.setImageBitmap(decodeSampleBitmapFromResource(getResources(),R.drawable.profilepictest,100,100));
+        try{
+            Bitmap bmp = encodeAsBitmap("Brandon Tran");
+            qrCodePic.setImageBitmap(bmp);
+        }
+        catch(WriterException e){
+            e.printStackTrace();
+        }
+
+        return view;
+
+    }
+    private static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight){
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if(height > reqHeight || width > reqWidth){
+            final int halfHeight = height/2;
+            final int halfWidth = width/2;
+
+            while((halfHeight / inSampleSize) >= reqHeight && (halfWidth / inSampleSize) >= reqWidth){
+                inSampleSize *= 2;
+            }
+        }
+        return inSampleSize;
+    }
+
+    public static Bitmap decodeSampleBitmapFromResource(Resources res, int resId, int reqWidth, int reqHeight){
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        options.inJustDecodeBounds = false;
+
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+
+    Bitmap encodeAsBitmap(String str) throws WriterException {
+        BitMatrix result;
+        Bitmap bitmap=null;
+        try
+        {
+            result = new MultiFormatWriter().encode(str,
+                    BarcodeFormat.QR_CODE, WIDTH, WIDTH, null);
+
+            int w = result.getWidth();
+            int h = result.getHeight();
+            int[] pixels = new int[w * h];
+            for (int y = 0; y < h; y++) {
+                int offset = y * w;
+                for (int x = 0; x < w; x++) {
+                    pixels[offset + x] = result.get(x, y) ? black:white;
+                }
+            }
+            bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+            bitmap.setPixels(pixels, 0, WIDTH, 0, 0, w, h);
+        } catch (Exception iae) {
+            iae.printStackTrace();
+            return null;
+        }
+        return bitmap;
+    }
+
+
+    public void onAttach(Context context){
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach(){
+        super.onDetach();
+    }
+
+
+}
+
