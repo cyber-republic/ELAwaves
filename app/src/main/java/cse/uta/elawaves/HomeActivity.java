eb03d3d2173544aa18c68ef578b76a5e5d34b56a
Brandon Tran <brandon.tran@mavs.uta.edu>
2019-04-01 06:14:33 -0500

    Bottom navigation bar transitions to fragment, Wallet Fragment not complete, so transitions to AccountInfoFragment instead

--- app/src/main/java/cse/uta/elawaves/HomeActivity.java
+++ app/src/main/java/cse/uta/elawaves/HomeActivity.java
@@ -1,14 +1,25 @@
 package cse.uta.elawaves;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.view.View;
+
+
 
 import org.elastos.carrier.FriendInfo;
 
+import cse.uta.elawaves.Fragments.AccountInfoFragment;
 import cse.uta.elawaves.Fragments.AddFriendFragment;
+import cse.uta.elawaves.Fragments.FriendInfoFragment;
 import cse.uta.elawaves.Fragments.FriendsFragment;
 
 public class HomeActivity extends AppCompatActivity implements FriendsFragment.OnFriendSelectedListener, AddFriendFragment.OnAddFriendFragmentListener {
@@ -19,15 +30,10 @@ public class HomeActivity extends AppCompatActivity implements FriendsFragment.O
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
         BottomNavigationView menu = findViewById(R.id.home_navigation);
+        menu.setOnNavigationItemSelectedListener(navListener);
 
-        menu.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                return false;
-            }
-        });
+
     }
 
     @Override
@@ -39,4 +45,29 @@ public class HomeActivity extends AppCompatActivity implements FriendsFragment.O
     public void onAddFriend(FriendInfo info) {
 
     }
+
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment selectedFragment = null;
+            switch(menuItem.getItemId()) {
+                case R.id.friends_item:
+                    selectedFragment = new FriendsFragment();
+                    break;
+                case R.id.myinfo_item:
+                    selectedFragment = new AccountInfoFragment();
+                    break;
+                case R.id.wallet_item:
+                    selectedFragment = new AccountInfoFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            return true;
+
+            }
+
+    };
+
 }
