a0c4d8f31142e718b672d48f5e46ba498910b916
ghebarb <ghebarb@gmail.com>
2019-04-06 19:04:20 -0500

    Moved DB to MessageManager - failed to pass context parameter so DB constructor still unused

--- app/src/main/java/cse/uta/elawaves/Messages/MessageManager.java
+++ app/src/main/java/cse/uta/elawaves/Messages/MessageManager.java
@@ -1,6 +1,7 @@
 package cse.uta.elawaves.Messages;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import org.elastos.carrier.exceptions.CarrierException;
@@ -13,10 +14,13 @@ import java.util.Observable;
 import cse.uta.elawaves.Carrier.CarrierImplementation;
 import cse.uta.elawaves.Database.DatabaseHandler;
 
+import static java.security.AccessController.getContext;
+
 public class MessageManager extends Observable {
 
     private static MessageManager instance;
     private static HashMap<String,ArrayList<Message>> messages = new HashMap<>();
+    DatabaseHandler dbHelper;
 
     public static MessageManager getInstance(){
         if(instance != null)
@@ -25,8 +29,32 @@ public class MessageManager extends Observable {
         return new MessageManager();
     }
 
-    private MessageManager(){
-        // pull all previous messages from database here
+    private MessageManager() {
+
+    }
+    private MessageManager(Context context){
+        dbHelper = new DatabaseHandler(context);
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        String conversation_query = "SELECT message, address, sent_received, message_timestamp" +
+                " FROM messages" +
+                " ORDER BY message_timestamp ASC;";
+        Cursor c = db.rawQuery(conversation_query, null);
+        if (c.moveToFirst()){
+            do {
+                // Passing values
+                String message = c.getString(0);
+                String address = c.getString(1);
+                String sent_recieved = c.getString(2);
+                Timestamp message_timestamp = Timestamp.valueOf(c.getString(3));
+
+                // Do something Here with values
+
+            } while(c.moveToNext());
+        }
+        c.close();
+        db.close();
+
     }
 
 
@@ -37,6 +65,13 @@ public class MessageManager extends Observable {
     public void addMessage(Message message){
         setChanged();
         messages.get(message.getAddress()).add(message);
+
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        String message_query = "INSERT INTO messages (message, address, sent_received, message_timestamp)" +
+                "VALUES " +
+                "(" + message.getMessage() + ", " + message.getAddress() + ", " + message.isReceived() + ", " + message.getMessageTimeStamp() + ");";
+        db.execSQL(message_query);
+
         notifyObservers(message);
     }
 
