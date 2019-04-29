68518d603d9f3446ec80fa17cf12d860a6cf3e10
Jimmy Chen <jchenex4@gmail.com>
2019-04-28 22:21:13 -0500

    Add files via upload

--- app/src/main/java/cse/uta/elawaves/Wallet/WalletImplementation.java
+++ app/src/main/java/cse/uta/elawaves/Wallet/WalletImplementation.java
@@ -6,26 +6,34 @@ import com.elastos.spvcore.ISubWallet;
 
 public class WalletImplementation {
 
-    private static WalletImplementation instance;
+    private static WalletImplementation instance = null;
     private MasterWalletManager walletManager;
     private IMasterWallet masterWallet;
     private ISubWallet isubWallet;
 
-    public static WalletImplementation getInstance(String path, String masterWalletId, String mnemonic, String phrasePassword, String payPassword)
+    public static WalletImplementation getWallet()
+    {
+        return instance;
+    }
+
+    public static WalletImplementation getInstance(String path, String masterWalletId, String phrasePassword, String payPassword)
     {
         if(instance == null)
-            instance = new WalletImplementation(path, masterWalletId, mnemonic, phrasePassword, payPassword);
+            instance = new WalletImplementation(path, masterWalletId, phrasePassword, payPassword);
 
         return instance;
     }
 
     private WalletImplementation(String path,
-                                 String masterWalletId, String mnemonic, String phrasePassword, String payPassword)
+                                 String masterWalletId, String phrasePassword, String payPassword)
     {
         String chainID = "ELA";
         long feePerKb = 10000;  // The unit of the parameter feePerKb is SELA
 
         walletManager = new MasterWalletManager(path);
+        String language = "english";
+        String mnemonic = walletManager.GenerateMnemonic(language);
+
         masterWallet = walletManager.CreateMasterWallet(masterWalletId, mnemonic, phrasePassword, payPassword,false);
         isubWallet = masterWallet.CreateSubWallet(chainID, feePerKb);
     }
@@ -36,6 +44,11 @@ public class WalletImplementation {
 
     public IMasterWallet CreateMasterWallet(String walletID,String mnemonic,String phrasePassword,String payPassword){
         return walletManager.CreateMasterWallet(walletID,mnemonic,phrasePassword,payPassword,false);
+    }
+
+    public long GetBalance()
+    {
+        return isubWallet.GetBalance();
     }
 
     public String CreateTransaction(String fromAddress, String toAddress, long amount, String memo, String remark)
